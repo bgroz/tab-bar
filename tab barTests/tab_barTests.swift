a10ab812df689d76c10057144e506614c65cec76
bgroz <Bugra@apple-MacBook-Pro.local>
2017-04-28 23:33:13 +0300

    Initial Commit

---
+++ tab barTests/tab_barTests.swift
@@ -0,0 +1,36 @@
+//
+//  tab_barTests.swift
+//  tab barTests
+//
+//  Created by Buğra on 28.04.2017.
+//  Copyright © 2017 Buğra Öz. All rights reserved.
+//
+
+import XCTest
+@testable import tab_bar
+
+class tab_barTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
